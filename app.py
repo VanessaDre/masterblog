e28bdf9b84a58cca896d008d8a924180db9e33a2
Vanessa <vanessa.dressler@hotmail.com>
2026-02-03 16:41:13 +0100

    Add delete route

--- app.py
+++ app.py
@@ -55,6 +55,16 @@ def add():
     return render_template("add.html")
 
 
+@app.route("/delete/<int:post_id>")
+def delete(post_id):
+    posts = load_posts()
+
+    posts = [post for post in posts if post.get("id") != post_id]
+
+    save_posts(posts)
+    return redirect(url_for("index"))
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
 
